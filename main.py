32f9f03dda054a79aa8c2ca370fde11ead824597
iZarrios <eyad_zarrios2001@yahoo.com>
2022-05-08 00:40:12 +0200

    first commit

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
-import colored
-
 # data structure that represents a node in the tree
-
-
 class Node():
-    def __init__(self, data):
-        self.data = data  # holds the key
-        self.parent = None
+    def __init__(self, data):  # inialize node
+        self.data = data
         self.left = None
         self.right = None
-        self.color = 1  # 0 Black, 1 Red
+        self.parent = None
+        self.color = 1  # 1 for red & 0 for black
 
 
-# class RedBlackTree implements the operations in Red Black Tree
+# Implements RBT
 class RedBlackTree():
     def __init__(self):
-        # rbt tree  begins with no nodes (TNULL  node)
         self.TNULL = Node(0)
-        self.TNULL.color = 0  # black color as from the rules of RBTree is for the node to be black
+        self.TNULL.color = 0
         self.TNULL.left = None
         self.TNULL.right = None
         self.root = self.TNULL
@@ -25,47 +20,42 @@ class RedBlackTree():
     def search(self, node, key):
         if node == self.TNULL or key == node.data:
             return node.data
+
         if key < node.data:
             return self.search(node.left, key)
         return self.search(node.right, key)
 
-    def fix_insert(self, k):
+    def fixInsert(self, k):
         while k.parent.color == 1:
             if k.parent == k.parent.parent.right:
                 u = k.parent.parent.left  # uncle
-                if u.color == 1:
-                    # case 3.1
+                if u.color == 1:  # Case 1
                     u.color = 0
                     k.parent.color = 0
                     k.parent.parent.color = 1
                     k = k.parent.parent
                 else:
-                    if k == k.parent.left:
-                        # case 3.2.2
+                    if k == k.parent.left:  # Case 2
                         k = k.parent
-                        self.right_rotate(k)
-                    # case 3.2.1
+                        self.rightRotate(k)
+                    # Case 3
                     k.parent.color = 0
                     k.parent.parent.color = 1
-                    self.left_rotate(k.parent.parent)
-            else:
-                u = k.parent.parent.right  # uncle
-
+                    self.leftRotate(k.parent.parent)
+            else:  # Exchange right & left
+                u = k.parent.parent.right
                 if u.color == 1:
-                    # mirror case 3.1
                     u.color = 0
                     k.parent.color = 0
                     k.parent.parent.color = 1
                     k = k.parent.parent
                 else:
                     if k == k.parent.right:
-                        # mirror case 3.2.2
                         k = k.parent
-                        self.left_rotate(k)
-                    # mirror case 3.2.1
+                        self.leftRotate(k)
                     k.parent.color = 0
                     k.parent.parent.color = 1
-                    self.right_rotate(k.parent.parent)
+                    self.rightRotate(k.parent.parent)
             if k == self.root:
                 break
         self.root.color = 0
@@ -73,7 +63,7 @@ class RedBlackTree():
     def searchTree(self, k):
         return self.search(self.root, k)
 
-    def left_rotate(self, x):
+    def leftRotate(self, x):
         y = x.right
         x.right = y.left
         if y.left != self.TNULL:
@@ -89,7 +79,7 @@ class RedBlackTree():
         y.left = x
         x.parent = y
 
-    def right_rotate(self, x):
+    def rightRotate(self, x):
         y = x.left
         x.left = y.right
         if y.right != self.TNULL:
@@ -111,7 +101,7 @@ class RedBlackTree():
         node.data = key
         node.left = self.TNULL
         node.right = self.TNULL
-        node.color = 1  # new node must be red
+        node.color = 1  # Insert red node not black to maintain bh
 
         y = None
         x = self.root
@@ -137,22 +127,46 @@ class RedBlackTree():
 
         if node.parent.parent == None:
             return
-        self.fix_insert(node)
 
-    def get_root(self):
+        self.fixInsert(node)
+
+    def getRoot(self):
         return self.root
 
     def height(self, node):
         if node == self.TNULL:
             return 0
-        lheight = self.height(node.left)
-        rheight = self.height(node.right)
-        return max(lheight, rheight) + 1
+        else:
+            lheight = self.height(node.left)
+            rheight = self.height(node.right)
+            if lheight > rheight:
+                return lheight + 1
+            else:
+                return rheight + 1
 
     def size(self, node):
         if node == self.TNULL:
             return 0
-        return self.size(node.left) + self.size(node.right) + 1
+        else:
+            return self.size(node.left) + self.size(node.right) + 1
+
+# Print level order traversal of tree
+
+
+def printLevelOrder(root, h):
+    for i in range(1, h+1):
+        printCurrentLevel(root, i)
+# Print nodes at a current level
+
+
+def printCurrentLevel(root, level):
+    if root is None:
+        return
+    if level == 1:
+        print(root.data, end=" ")
+    elif level > 1:
+        printCurrentLevel(root.left, level-1)
+        printCurrentLevel(root.right, level-1)
 
 
 if __name__ == "__main__":
@@ -166,48 +180,53 @@ if __name__ == "__main__":
     rbt = RedBlackTree()
 
     try:
+
         dictf = open('EN-US-Dictionary.txt', 'r')
-        print("Loaded Dictionary Successfully")
+        dictionary = dictf.read().splitlines()
+        for key in dictionary:
+            rbt.insert(key.lower())
+        print(P+"\nLoaded Dictionary Successfully"+W)
     except:
         print(R+"\nCouldn't open dictionary\n"+W)
         exit(1)
 
-    dictionary = dictf.read()
-    # print(dictionary)
-    # exit(0)
-    for key in dictionary:
-        rbt.insert(key)
     while True:
-        print("1-Search\n2-Insert\n3-Tree Height\n4-Tree Size\n5-Exit")
+        print('\n----------Kindly, Choose from the follwing options----------\n')
+        print("1 - Search\n2 - Insert\n3 - Tree Height\n4 - Tree Size\n5 - Show (Level order traversal)\n6 - Exit")
         choice = input("> ")
         if choice.lower() == 'search' or choice.lower() == "1":
-            key = input("Search for Key: ")
-            if key == '':
-                continue
+            key = input("Search for Key: ").lower()
             if rbt.searchTree(key) == 0:
-                print("NO")
-            else:
                 print("YES")
+            else:
+                print("NO")
         elif choice.lower() == 'insert' or choice.lower() == "2":
-            # print(f"Tree Height before Insertion: {rbt.height(rbt.get_root())}")
-            # print(f"Tree Size before insertion: {rbt.size(rbt.get_root())}")
-            key = input("Insert Key: ")
+            print("Tree Height before Insertion: {}".format(
+                rbt.height(rbt.getRoot())))
+            print("Tree Size before insertion: {}".format(
+                rbt.size(rbt.getRoot())))
+            key = input("Insert Key: ").lower()
             if rbt.searchTree(key) == 0:
                 rbt.insert(key)
 
-                print(f"{key} Inserted!")
-                # print("Tree Height after Insertion: {rbt.height(rbt.get_root())}")
-                # print(f"Tree Size after insertion: {rbt.size(rbt.get_root())}")
+                print("{} Inserted!".format(key))
+                print("Tree Height after Insertion: {}".format(
+                    rbt.height(rbt.getRoot())))
+                print("Tree Size after insertion: {}".format(
+                    rbt.size(rbt.getRoot())))
             else:
-                print("Word already Exists!")
+                print("ERROR: Word already in the dictionary!")
         elif choice.lower() == 'tree height' or choice.lower() == "3":
-            print(f"Tree Height: {rbt.height(rbt.get_root())}")
+            print("Tree Height: {}".format(rbt.height(rbt.getRoot())))
         elif choice.lower() == 'tree size' or choice.lower() == "4":
-            print(f"Tree Size: {rbt.size(rbt.get_root())}")
-            # ".format(rbt.size(rbt.get_root())))
+            print("Tree Size: {}".format(rbt.size(rbt.getRoot())))
 
-        elif choice.lower() == 'exit' or choice.lower() == "5":
-            print("------Terminating Program------")
-            exit(0)
+        elif choice.lower() == 'show' or choice.lower() == "5":
+            printLevelOrder(rbt.getRoot(), rbt.height(rbt.getRoot()))
+            break
+
+        elif choice.lower() == 'exit' or choice.lower() == "6":
+            print("----------Terminating Program----------")
+            break
         else:
             print("Not a Valid Choice!")
